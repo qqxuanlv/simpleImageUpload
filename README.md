174e467f113abe1256209a39b16beb771dc56891
qqxuanlv <834337625@qq.com>
2019-07-22 16:11:59 +0800

    s

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
 # simpleImageUpload
 简单多图片上传
+
+![1563781594193](./md/1563781594193.png)
+
+ 
+
+![1563782654791](./md/1563782654791.png)
+
+初始化 默认上传容量为5 
+
+上传的时候已经改好filename为uuid
+
+
+
+## 初始化：
+
+### 外部依赖
+
+```html
+ <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.slim.min.js"</script>
+```
+
+### css样式
+
+```css
+ <link href="css/simpleImageUpload.min.css" rel="stylesheet">
+```
+
+### js
+
+```javascript
+ <script src="js/simpleImageUpload.js"></script>
+ //位置
+ <script>
+ 	//上传操作
+ </script>
+ </body>
+```
+
+
+
+
+
+
+
+```javascript
+<script>
+
+	//init 默认容量为5 不能小于1
+	   myFileUpload.init(5);
+	
+</script>
+```
+
+
+
+
+
+### 获取所有文件：
+
+```javascript
+<script>
+    //两种方法获取
+    
+    //1.获取文件 返回格式是map ,key:重命名之后的uuid(不带后缀) ，value:所对应的文件
+    
+    	 myFileUpload.getFilesMap();
+    
+    //2.获取文件 返回格式是数组, item项是所对应的文件。
+     	 myFileUpload.getFilesArray();
+    
+</script>
+```
+
+
+
+### upload:
+
+```javascript
+<script>
+    
+	//自定制上传,upload 回调 参数是 myFileUpload.getFilesArray();
+	myFileupload.upload();
+</script>
+```
+
+
+
+### 例子：
+
+```html
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="css/simpleImageUpload.min.css" rel="stylesheet">
+	<script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.slim.min.js"></script>
+
+</head>
+<body>
+    
+    <div id="filePosition">
+
+    </div>
+    
+    <button onclick="upload()">上传</button>
+    <script src="js/simpleImageUpload.js"></script>
+
+    <script>
+    function upload(){
+		 myFileUpload.upload(function(res){
+			console.log(res);
+		 })
+        }
+    </script>
+</body>
+</html>
+
+```
+
+![1563783017919](./md/1563783017919.png)
---
+++ js/simpleImageUpload.js
@@ -0,0 +1,154 @@
+var myFileUpload = newFunction()
+ 
+function newFunction() {
+    return {
+        tmpFile: null,
+        formUploadData: null,
+        insertBtn: null,
+        insertBtn2: null,
+        initCapacity:5,
+        init: function (capacity) {
+
+            if(capacity != null ){
+                if(parseInt(capacity)>0){
+                    this.initCapacity=capacity;
+                }
+            } 
+            this.formUploadData = new FormData();
+            $("#filePosition").append(`
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            图片上传
+                        </h3>
+                    </div>
+                    <div id="formUpload" class="panel-body">
+                        <div class="fileAdd">
+                        </div>
+                    </div>
+                </div>
+                
+                <input id="multipleFile" style="display: none" name="file" accept="image/*" type="file">
+                <button class="fileAdd btn btn-success pull-right btn2" style="display: none">上传文件</button>`);
+            this.tmpFile = $("#multipleFile");
+            this.insertBtn = $("div.fileAdd");
+            this.insertBtn2 = $("button.fileAdd");
+            $(".fileAdd").click(function () {
+                $("#multipleFile").click();
+            });
+            let that = this;
+            $("#multipleFile").change("propertychange", function (e) {
+                if ($(this)[0].files == null)
+                    return;
+                let fileItem = $(this)[0].files[0];
+                //回显操作
+                //添加操作
+                let newFileName = that.rename(fileItem.name);
+                fileItem.name = newFileName.name;
+                let newNameFile = new File([fileItem], newFileName.name);
+                
+                let capacityCount=0;
+                for (kes of that.formUploadData.entries()) {
+                    capacityCount++;
+                }
+                if(capacityCount>=that.initCapacity){
+                    console.log("已经达到最大容量");
+                    return ; 
+                }
+                
+                that.formUploadData.append(newFileName.hash, newNameFile);
+
+
+
+                //上传按钮
+                let fileCount = 0;
+                for (kes of that.formUploadData.entries()) {
+                    fileCount++;
+                }
+                if (fileCount > 0) {
+                    that.insertBtn.hide();
+                    that.insertBtn2.show();
+                }
+                else {
+                    console.log(fileCount);
+                    that.insertBtn.show();
+                    that.insertBtn2.hide();
+                }
+                that.insertImg(fileItem, newFileName.hash);
+                that.tmpFile.files = null;
+            });
+        },
+        getFilesMap: function () {
+            let map = new Map();
+            for (item of this.formUploadData.entries()) {
+                map.set(item[0], item[1]);
+            }
+            return map;
+        },
+        getFilesArray: function () {
+            let array = new Array();
+            for (item of this.formUploadData.entries()) {
+               array.push(item[1]);
+            }
+            return array;
+        },
+        upload: function (methodProxy) {
+            let array = this.getFilesArray();
+            if (array.length < 1) {
+                console.log("需要添加文件");
+                throw "当前文件为空";
+            }
+            methodProxy(array);
+        },
+        insertImg: function (file, hash) {
+            if (file == null)
+                throw "文件回显：文件不能为空!";
+            let that = this;
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadstart = function () {
+           
+            };
+            reader.onload = function (e) {
+                let newDom = `<div class="img-container"> <img    src="` + reader.result + `"> <div class="operate" style="display:none"> <div class="removeIcon" onclick="myFileUpload.removeFile(this,'` + hash + `')"> </div> </div></div>`;
+                $("#formUpload").append(newDom);
+                that.bindHover();
+            };
+            that.tmpFile.val("");
+        },
+        rename: function (txt) {
+            function guid2() {
+                function S4() {
+                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                }
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+            function getExt(txt) {
+                return txt.substring(txt.lastIndexOf("."));
+            }
+            let hash = guid2();
+            return { hash: hash, name: hash + getExt(txt) };
+        },
+        removeFile: function (dom, hash) {
+            $(dom).parents(".img-container").remove();
+            this.formUploadData.delete(hash);
+            let count = 0;
+            for (item of this.formUploadData.keys()) {
+                count++;
+            }
+            if (count == 0) {
+                this.insertBtn.show();
+                this.insertBtn2.hide();
+            }
+        },
+        bindHover: function () {
+            $(".img-container").hover(function () {
+                $(this).find(".operate").show();
+            }, function () {
+                $(this).find(".operate").hide();
+            });
+        }
+    };
+}
+ 
+myFileUpload.init();
